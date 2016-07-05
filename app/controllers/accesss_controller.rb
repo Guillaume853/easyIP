58df8569893f81324445b54bde24e445d4e5b4d3
Guillaume853 <autierguillaume1@gmai.com>
2016-07-06 00:52:39 +0200

    add start_date tracker in accesses and put access list into table form

--- app/controllers/accesss_controller.rb
+++ app/controllers/accesss_controller.rb
@@ -3,7 +3,7 @@ class AccesssController < ApplicationController
   def create
     @roadshow = Roadshow.find(params[:roadshow_id])
     if (@roadshow.user != current_user)
-      Access.create(user: current_user, roadshow: @roadshow)
+      Access.create(user: current_user, roadshow: @roadshow, start_access: DateTime.now)
     end
 #  cette syntaxe est recommandée mais est moins intuitive:  @roadshow.accesss.create! user: current_user
     redirect_to roadshow_path(@roadshow)
