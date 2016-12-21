ac6f3fc9e2a2a384908ef8f5d44bb02652e83ff6
Guillaume853 <autierguillaume1@gmai.com>
2016-12-21 23:54:32 +0100

    fine tuning of xls download

--- app/models/latestpage.rb
+++ app/models/latestpage.rb
@@ -7,7 +7,7 @@ class Latestpage < ApplicationRecord
 
 
   def self.to_csv(options = {})
-    desired_columns = ["id"]
+    desired_columns = ["id", "user.email"]
     CSV.generate(options) do |csv|
       csv << column_names
       all.each do |latestpage|
