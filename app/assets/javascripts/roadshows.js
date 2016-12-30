eea1fdbdb27cf2673928bf78cc6499d6ce57bf10
Guillaume853 <autierguillaume1@gmai.com>
2016-12-30 17:55:52 +0100

    updated navbar design

--- app/assets/javascripts/roadshows.js
+++ app/assets/javascripts/roadshows.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
       $(".navbar-wagon").addClass("navbar-transparent");
     }
   });
-
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -30,7 +29,6 @@ $(document).ready(function(){
       }
     });
   });
-
 });
 
 
