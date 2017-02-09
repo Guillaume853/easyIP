6b0a76c11c4889ec5010d96b2df70a44a8764c04
Guillaume853 <autierguillaume1@gmai.com>
2017-02-09 12:52:18 +0100

    changed full screen view design

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -13,24 +13,61 @@ class ChargesController < ApplicationController
     @latestpage = Latestpage.new
 
     # Amount in cents
-    @amount = 990
+    @amount = 9900
+    @final_amount = @amount
 
+    @code = params[:couponCode]
 
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
+      if !@code.blank?
+        @discount = get_discount(@code)
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'eur'
-    )
+        if @discount.nil?
+          flash[:error] = 'Coupon code is not valid or expired.'
+          redirect_to new_charge_path
+          return
+        else
+          @discount_amount = @amount * @discount
+          @final_amount = @amount - @discount_amount.to_i
+        end
+
+        charge_metadata = {
+          :coupon_code => @code,
+          :coupon_discount => (@discount * 100).to_s + "%"
+        }
+      end
+
+      charge_metadata ||= {}
+
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
+      Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @final_amount,
+        :description => 'Investor presentation for '+ @roadshow.company,
+        :currency    => 'eur',
+        :metadata    => charge_metadata
+      )
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
 
+  private
+
+  COUPONS = {
+    'TEST' => 0.9,
+    'PIPPA' => 0.9
+  }
+
+  def get_discount(code)
+    # Normalize user input
+    code = code.gsub(/\s+/, '')
+    code = code.upcase
+    COUPONS[code]
+  end
+
 end
+
